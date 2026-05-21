e260714d5461c4fac84f1eb148adf1adf69db1f5
新大陆苍蓝星 <1434012910@qq.com>
2026-05-21 20:56:19 +0800

    categoryGroup.pug  catalog_list.js 更新文件配置

--- themes/anzhiyu/scripts/helpers/catalog_list.js
+++ themes/anzhiyu/scripts/helpers/catalog_list.js
@@ -1,9 +1,11 @@
 hexo.extend.helper.register("catalog_list", function (type) {
+  const url_for = this.url_for;
   let html = ``;
   hexo.locals.get(type).map(function (item) {
+    const link = url_for(item.path);
     html += `
-    <div class="catalog-list-item" id="/${item.path}">
-      <a href="/${item.path}">
+    <div class="catalog-list-item" id="${link}">
+      <a href="${link}">
         ${item.name}
       </a>
     </div>
